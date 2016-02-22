6d397ec48ffdcaf62508198dd2825b390289651b
Spencer Powell <spenc84@gmail.com>
2016-02-22 12:56:06 -0600

    initial commit

--- inventoryCtrl.js
+++ inventoryCtrl.js
@@ -1,14 +1,49 @@
-var items = [
-	  {
-		  name: 'rotten log'
-		, price: 12.99
-	  }
-	, {
-		  name: 'single chipped brick'
-		, price: 45
-	  }
-	, {
-		  name: 'left shoe, size 15'
-		, price: 25
-	  }
-];
+// var items = [
+// 	  {
+// 		  name: 'rotten log'
+// 		, price: 12.99
+// 	  }
+// 	, {
+// 		  name: 'single chipped brick'
+// 		, price: 45
+// 	  }
+// 	, {
+// 		  name: 'left shoe, size 15'
+// 		, price: 25
+// 	  }
+// ];
+
+angular.module('myEmporium')
+.controller('inventoryCtrl', function( $scope ){
+	$scope.test = "I'm Alive!";
+
+	$scope.items = [
+		  {
+			  name: 'rotten log'
+			, price: 12.99
+		  }
+		, {
+			  name: 'single chipped brick'
+			, price: 45
+		  }
+		, {
+			  name: 'left shoe, size 15'
+			, price: 25
+		  }
+	];
+
+	$scope.addItem = function() {
+		$scope.items.push({name: $scope.newItem.name, price: $scope.newItem.price});
+		$scope.newItem.price = null;
+		$scope.newItem.name = '';
+	}
+
+	$scope.removeItem = function( itemIndex ) {
+		$scope.items.splice(itemIndex, 1);
+	}
+
+	$scope.hiddenItem = '';
+	// $scope.hideItem = function( item ){
+	// 	$scope.hiddenItem = item;
+	// }
+});
