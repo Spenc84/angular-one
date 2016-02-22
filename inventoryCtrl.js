bebd5a9c30dd69bfbece729f142c25d81a6e4bbf
Ryan Walsh <ryan@thehatrack.net>
2016-02-22 11:35:49 -0600

    after lecture

--- inventoryCtrl.js
+++ inventoryCtrl.js
@@ -1,14 +1,36 @@
-var items = [
-	  {
-		  name: 'rotten log'
-		, price: 12.99
-	  }
-	, {
-		  name: 'single chipped brick'
-		, price: 45
-	  }
-	, {
-		  name: 'left shoe, size 15'
-		, price: 25
-	  }
-];
+angular.module('myEmporium')
+.controller('inventoryCtrl', function( $scope ) {
+
+	$scope.items = [
+		  {
+			  name: 'rotten log'
+			, price: 12.99
+		  }
+		, {
+			  name: 'single chipped brick'
+			, price: 45
+		  }
+		, {
+			  name: 'left shoe, size 15'
+			, price: 25
+		  }
+	];
+
+	$scope.hideItem = function( name ) {
+		$scope.hiddenItem = name;
+	}
+
+	$scope.addItem = function() {
+		$scope.items.push({
+			  name: $scope.newItem.name
+			, price: $scope.newItem.price
+		});
+		$scope.newItem.price = null;
+		$scope.newItem.name = '';
+	}
+
+	$scope.removeItem = function( itemIndex ) {
+		$scope.items.splice( itemIndex, 1 );
+	}
+
+});
